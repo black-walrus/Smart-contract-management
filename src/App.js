84f8b00809d09b5d55f27531b5d17300a0b11c51
Seth Gruspe <201911598@fit.edu.ph>
2024-10-04 13:49:46 +0800

    feature: implemented viewing of pokemon collection

--- src/App.js
+++ src/App.js
@@ -138,11 +138,59 @@ function App() {
     } 
   }
 
+  const createPokemonNFTCard = async (index) => {
+    const POKE_API_URL = "https://pokeapi.co/api/v2/pokemon"; 
+    const card = document.createElement('div');
+    card.className = "pokemon-nft-card";
+
+    const pokemonName = document.createElement('h1');
+    const pokemonImage = document.createElement('img');
+    const pokemonLevel = document.createElement('h3');
+    const pokemonType = document.createElement('h3');
+    const pokemonProfile = document.createElement("div");
+
+    pokemonProfile.className = "pokemon-nft-profile";
+
+    const _name = await contract.methods.getPokemon(index).call({from: account});
+    const _level = await contract.methods.getPokemonLevel(index).call({from: account});
+    const _type1 = await contract.methods.getPokemonType1(index).call({from: account});
+    const _type2 = await contract.methods.getPokemonType2(index).call({from: account});
+
+    fetch(`${POKE_API_URL}/${_name.toLowerCase()}`)
+      .then((response) => response.json())
+      .then((pokemon) => {
+        pokemonName.textContent = _name;
+        pokemonImage.src = pokemon.sprites.other.dream_world.front_default;
+        pokemonLevel.textContent = `Level: ${_level}`;
+        pokemonType.textContent = `Types: ${_type1}`;
+
+        pokemonImage.height = 250;
+        pokemonImage.width = 250; 
+
+        if (_type2 != "NONE") {
+          pokemonType.textContent += `/${_type2}`;
+        }
+
+        card.appendChild(pokemonName);
+        card.appendChild(pokemonImage);
+
+        pokemonProfile.appendChild(pokemonLevel);
+        pokemonProfile.appendChild(pokemonType); 
+
+        card.appendChild(pokemonProfile);
+        
+        document.getElementById('pokemon-nft-cards-container').appendChild(card)
+      })
+    }
+  
   const viewCollection = async () => {
     try {
-      const collectionSize = 2; 
-      const collection = await contract.methods.pokemonNFTS().call({from: account});
-      console.log(collection.length);
+      const cardsContainer = document.getElementById('pokemon-nft-cards-container');
+      const collectionSize = 4; 
+
+      for (let i = 0; i < collectionSize; i++) { 
+        createPokemonNFTCard(i); 
+      }
     } catch (error) {
       console.error("Error getting Pokemon Collection: ", error);
     }
@@ -157,12 +205,17 @@ function App() {
             <button onClick={mintPokemon}>Mint Pokemon</button>
             <button onClick={viewCollection}> View Pokemon Collection </button> 
           </div>
-          
+
           <p>Account: {account}</p>
           <h2 id="mint-pokemon"></h2>
           <div id="mint-pokemon-container"></div>
           <p id="pokemon-info"></p>
           <div id="pokemon-container"></div>
+
+          <div id="pokemon-nft-cards-container">
+
+          </div>
+
         </div>
         <style jsx> 
           {`
